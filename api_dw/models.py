c196afa269404173e92957d20c6347ea03b02df0
Muhammad Syaiful Aliim <muhammad.syaiful.aliim@unsoed.ac.id>
2024-05-05 21:58:21 +0700

    commit new model

--- api_dw/models.py
+++ api_dw/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class MasaStudi(models.Model):
+    nim = models.CharField(max_length=9)
+    nama = models.CharField(max_length=64)
+    sks = models.IntegerField()
+    ip = models.DecimalField(decimal_places=2, max_digits=5)
+    masa_studi = models.FloatField()
+    status = models.BooleanField()
+
+    def __str__(self):
+        return str(self.nim)
+
+
+class KetepatanLulus(models.Model):
+    nim = models.CharField(max_length=9)
+    nama = models.CharField(max_length=64)
+
+    def __str__(self):
+        return str(self.nim)
